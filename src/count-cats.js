ff5c5adf636d73b3d614ec7b2b8bdb1cc4e14bfa
Dimon <Dimon@gs.by>
2020-02-25 17:37:37 +0300

    feat: finish algorithm for 'count-cats' and 'dream-team'

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  let findElem = '^^';
+  if (!matrix || !matrix.length || matrix !== undefined) {
+    matrix.forEach(array => {
+      array.forEach(el => {
+        (el === findElem) ? count++ : 0;
+      })
+    });
+    return count;
+  } else {
+    return 0;
+  }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,8 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  return members.filter(element => {
+    return typeof element === 'string';
+  }).map(element =>
+    element.trim().toUpperCase().slice(0, 1)
+  ).sort().join('');
 };
