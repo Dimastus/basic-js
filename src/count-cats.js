2d12993cd9ef0be15f54ba2d5876f8b74e4bbf0d
Dimon <Dimon@gs.by>
2020-02-28 17:43:21 +0300

    feat: add function count-cats and transform-array

--- src/count-cats.js
+++ src/count-cats.js
@@ -6,9 +6,20 @@ module.exports = function countCats(matrix) {
       array.forEach(el => {
         (el === findElem) ? count++ : 0;
       })
-    });
+    }); //47ms
     return count;
   } else {
     return 0;
   }
+
+  /*
+  if (!matrix || !matrix.length || matrix !== undefined) {
+    // const flatSingle = arr => [].concat(...arr);
+    // matrix = flatSingle(matrix); //47ms
+    // matrix = matrix.reduce((acc, val) => acc.concat(val), []); //62ms
+    return matrix.filter(el => el === '^^').length;
+  } else {
+    return 0;
+  }
+  */
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,62 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (Array.isArray(arr)) {
+        let result = [];
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i]) {
+                case '--discard-next':
+                    if (arr[i + 1]) i++;
+                    break;
+                case '--discard-prev':
+                    // (arr[i - 1]) ? arr.splice(i - 1, 2): arr.splice(i, 1);
+                    // if (arr[i - 1]) result.splice(i - 1, 1);
+                    if (arr[i - 1]) result.pop();
+                    break;
+                case '--double-next':
+                    // (arr[i + 1]) ? arr.splice(i, 1, arr[i + 1]): arr.splice(i, 1);
+                    if (arr[i + 1]) result.push(arr[i + 1]);
+                    break;
+                case '--double-prev':
+                    // if (arr[i - 1]) {
+                    //     arr.splice(i, 1);
+                    //     arr.splice(i, 0, i);
+                    // } else {
+                    //     arr.splice(i, 1);
+                    // }
+                    if (arr[i - 1]) result.push(arr[i - 1]);
+                    break;
+                default:
+                    result.push(arr[i]);
+                    break;
+            }
+        }
+        return result;
+    } else {
+        throw new Error();
+    }
+    /*if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--discard-next') {
+            if (i < arr.length - 1) {
+                i++;
+            }
+        } else if (arr[i] == '--discard-prev') {
+            if (arr2.length > 0) {
+                arr2.pop();
+            }
+        } else if (arr[i] == '--double-next') {
+            if (i < arr.length - 1) {
+                arr2.push(arr[i + 1]);
+            }
+        } else if (arr[i] == '--double-prev') {
+            if (i > 0) {
+                arr2.push(arr[i - 1]);
+            }
+        } else {
+            arr2.push(arr[i]);
+        }
+    }
+    return arr2;*/
 };
