020c844f67fc2ade6210ec4ea393727a068c2210
Dimon <Dimon@gs.by>
2020-03-10 14:13:29 +0300

    feat: complete task 'extended-repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,15 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let {repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = ''} = options,
+    result = '';
+    addition = String(addition);
+    for (let i = 0; i < repeatTimes; i++) {
+        result += `${str}${addition}`;
+        for (let j = 0; j < additionRepeatTimes - 1; j++) {
+            result += `${additionSeparator}${addition}`;
+        }
+        if (repeatTimes >= 1 && i < repeatTimes - 1) {
+            result += `${separator}`;
+        }
+    }
+    return result;
 };
-  
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,12 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, deep = 0, max = 0) {
+        if (Array.isArray(arr)) {
+            deep++;
+            for (let i = 0; i < arr.length; i++) {
+                this.calculateDepth(arr[i], deep);
+            }
+            deep > max ? max = deep : deep = 1;
+        }
+        return max;
     }
 };
