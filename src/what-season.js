3d2b78ec961cc67c2f108f7277b9bd7897441b3b
Dimon <Dimon@gs.by>
2020-03-10 16:57:56 +0300

    feat: complete task 'what season'

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,24 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  } else if (date.hasOwnProperty('getMonth')) {
+    throw new Error();
+  } else {
+    let period = {
+      11: 'winter',
+      0: 'winter',
+      1: 'winter',
+      2: 'spring',
+      3: 'spring',
+      4: 'spring',
+      5: 'summer',
+      6: 'summer',
+      7: 'summer',
+      8: 'autumn',
+      9: 'autumn',
+      10: 'autumn'
+    };
+
+    return period[date.getMonth()];
+  }
 };
