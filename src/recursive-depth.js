0bd4ac16d99210d024ba7a6c08944a8978c39600
Dimon <Dimon@gs.by>
2020-03-13 11:56:59 +0300

    feat: complete tasks 'carbon dating', 'hanoi tower', 'recursive depth', 'vigenere cipher'

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,12 +1,13 @@
 module.exports = class DepthCalculator {
-    calculateDepth(arr, deep = 0, max = 0) {
-        if (Array.isArray(arr)) {
-            deep++;
-            for (let i = 0; i < arr.length; i++) {
-                this.calculateDepth(arr[i], deep);
-            }
-            deep > max ? max = deep : deep = 1;
+    calculateDepth(arr) {
+         let scope = this;
+         if (Array.isArray(arr)) {
+            return arr.reduce(function (previous, current) {
+                let flat = scope.calculateDepth(current);
+                return (previous > flat) ? previous : (1 + flat);
+            }, 1);
+        } else {
+            return 0;
         }
-        return max;
     }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,49 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direction = true) {
+        this.direction = direction;
+    }
+    encrypt(str, key) {
+        if (!str || !key) throw new Error();
+        let message = str.toUpperCase(),
+            spliceMess = message.replace(/(\W|\d)/g, ''),
+            keyUpper = key.toUpperCase(),
+            result = '',
+            alphabetLength = 26; //65 - 90
+        for (let i = 0, j = 0; i < message.length; i++) {
+            if (/^(\W|\d)$/.test(message[i])) {
+                result += message[i];
+                continue;
+            }
+            let symbolMessage = (spliceMess[(j >= spliceMess.length) ? j % spliceMess.length : j]).charCodeAt(0),
+                symbolKey = keyUpper[(j >= keyUpper.length) ? j % keyUpper.length : j],
+                ki = (typeof alphabetLength == 'undefined') ? parseInt(symbolKey) : (symbolKey.charCodeAt(0)),
+                symbolTable = String.fromCharCode(((alphabetLength + ((symbolMessage) + (ki))) % alphabetLength) + 65);
+            result += symbolTable;
+            j++;
+        }
+        return (this.direction) ? result : result.split('').reverse().join('');
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(str, key) {
+        if (!str || !key) throw new Error();
+        let message = str.toUpperCase(),
+            spliceMess = message.replace(/(\W|\d)/g, ''),
+            keyUpper = key.toUpperCase(),
+            result = '',
+            alphabetLength = 26; //65 - 90
+        for (let i = 0, j = 0; i < message.length; i++) {
+            if (/^(\W|\d)$/.test(message[i])) {
+                result += message[i];
+                continue;
+            }
+            let symbolMessage = (spliceMess[(j >= spliceMess.length) ? j % spliceMess.length : j]).charCodeAt(0),
+                symbolKey = keyUpper[(j >= keyUpper.length) ? j % keyUpper.length : j],
+                ki = (typeof alphabetLength == 'undefined') ? parseFloat(symbolKey) : (symbolKey.charCodeAt(0)),
+                symbolTable = String.fromCharCode(((alphabetLength + ((symbolMessage) + (-ki))) % alphabetLength) + 65);
+            result += symbolTable;
+            j++;
+        }
+        return (this.direction) ? result : result.split('').reverse().join('');
     }
 }
 
